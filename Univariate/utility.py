16e5bfea62601ad7a66b6e02081029bb51923beb
Thanigaivel G <65648630+ThanigaivelGanapathi@users.noreply.github.com>
2025-02-15 18:15:11 +0530

    Add files via upload

--- Univariate/utility.py
+++ Univariate/utility.py
@@ -1,3 +1,4 @@
+import pandas as pd
 class Univariate:
     def qualQuan(self,dataset):
         qual = []
@@ -7,4 +8,12 @@ class Univariate:
                 qual.append(column)
             else:
                 quan.append(column)
-        return qual,quan        
+        return qual,quan
+
+    def getMeanMedianMode(self,dataset,quan):
+        tble = pd.DataFrame(index = ["Mean","Median","Mode"],columns = quan)
+        for column in quan:             
+            tble[column]["Mean"] = dataset[column].mean() 
+            tble[column]["Median"] = dataset[column].median() 
+            tble[column]["Mode"] = dataset[column].mode()[0]
+        return tble    
